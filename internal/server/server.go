56e3849daacce24cdc2dc4d573f87cbe1fec00f2
arloor <admin@arloor.com>
2023-05-21 15:04:57 +0800

    支持connect

--- internal/server/server.go
+++ internal/server/server.go
@@ -13,9 +13,12 @@ func Serve() error {
 	instance := config.Instance
 	handler := MineHandler{}
 	srv := &http.Server{
-		Addr:         instance.Addr,
-		Handler:      handler,
-		WriteTimeout: 30 * time.Second, // Set idle timeout
+		Addr:              instance.Addr,
+		Handler:           handler,
+		IdleTimeout:       30 * time.Second,
+		ReadHeaderTimeout: 30 * time.Second,
+		ReadTimeout:       30 * time.Second,
+		WriteTimeout:      30 * time.Second, // Set idle timeout
 	}
 	if !instance.UseTls {
 		return srv.ListenAndServe()
