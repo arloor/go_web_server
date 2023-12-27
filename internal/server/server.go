b64a459e2a2e4b52728e53d5c09c0c7f043cb1bc
arloor <admin@arloor.com>
2023-12-27 13:15:31 +0800

    schedule refresh cert

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,11 +1,17 @@
 package server
 
 import (
+	"crypto/tls"
 	"go_web_server/internal/config"
+	"log"
 	"net/http"
 	"time"
-	"crypto/tls"
 )
+
+var ssl_cert *tls.Certificate = nil
+var ssl_last_cert_update time.Time = time.Now()
+
+const ssl_cert_update_interval = 5 * time.Hour
 
 func Serve() error {
 	http.HandleFunc("/ip", writeIp)
@@ -22,13 +28,26 @@ func Serve() error {
 		WriteTimeout:      30 * time.Second, // Set idle timeout
 		TLSConfig: &tls.Config{
 			GetCertificate: func(*tls.ClientHelloInfo) (*tls.Certificate, error) {
-                                               // Always get latest localhost.crt and localhost.key 
-                                               // ex: keeping certificates file somewhere in global location where created certificates updated and this closure function can refer that
-				cert, err := tls.LoadX509KeyPair(instance.Cert, instance.PrivKey)
-				if err != nil {
-					return nil, err
+				// Always get latest localhost.crt and localhost.key
+				// ex: keeping certificates file somewhere in global location where created certificates updated and this closure function can refer that
+				now := time.Now()
+				if ssl_cert == nil || now.Sub(ssl_last_cert_update) > ssl_cert_update_interval {
+					cert, err := tls.LoadX509KeyPair(instance.Cert, instance.PrivKey)
+					if err != nil {
+						log.Println("Error loading certificate", err)
+						if ssl_cert != nil {
+							return ssl_cert, nil
+						}
+						return nil, err
+					} else {
+						log.Println("Loaded certificate", instance.Cert, instance.PrivKey)
+					}
+					ssl_cert = &cert
+					ssl_last_cert_update = now
+					return &cert, nil
+				} else {
+					return ssl_cert, nil
 				}
-				return &cert, nil
 			},
 		},
 	}
